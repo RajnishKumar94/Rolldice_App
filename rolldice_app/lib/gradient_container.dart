2e919adec3370e4a77c9558e232c942987a68f7e
RajnishKumar94 <rajnishkumar9344rk@gmail.com>
2024-09-11 20:23:14 +0530

    first commit

---
+++ rolldice_app/lib/gradient_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+import 'dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+          child:
+DiceRoller()
+          //  StyledText('Roll Dice'),
+          ),
+    );
+  }
+}
